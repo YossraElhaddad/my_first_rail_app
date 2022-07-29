f6a2fdf6443b42079f2d66a8eebed423e3b4b529
YossraElHaddad <nspecial56@gmail.com>
2022-07-29 16:52:39 +0200

    updates for heroku deployment

--- app/assets/config/routes.js
+++
@@ -1,9 +0,0 @@
-//= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
-
-Rails.application.routes.draw do
-        root 'cars#index'
-resources: cars
-end
